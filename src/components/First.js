ebb219a79f017e3096226dde37e9356cfcecc00c
omargargouri1 <omar2gargouri@gamil.com>
2022-11-21 10:30:20 +0100

    adding-chekpoint

---
+++ src/components/First.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const First = () => {
+  return (
+    <div style={{backgroundColor:"red"}}>
+          <h1>hello</h1>
+    </div>
+  )
+}
+
+export default First
+
---
+++ src/components/Second.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Second = () => {
+  return (
+    <div style={{backgroundColor:"blueviolet"}}>
+          <p>this is our first react app</p>
+    </div>
+  )
+}
+
+export default Second
